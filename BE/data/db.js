b67373223c17693f2a5f3ba1ca1265b7606af6a4
Damiano-Mariscoli <damiano.mariscoli2@gmail.com>
2025-02-04 12:54:55 +0100

    asd

--- BE/data/db.js
+++ BE/data/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  // host: process.env.DB_HOST || "localhost",
+  // user: process.env.DB_USER || "root",
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_NAME,
+  url: process.env.DATABASE_URL,
 });
 
 connection.connect((err) => {
